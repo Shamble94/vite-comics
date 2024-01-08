805715f9b141fc150734176a8dde45b4f20a2aa8
Shamble94 <fratus.nicholas@gmail.com>
2024-01-08 17:53:15 +0100

    AppMain creato

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<script>
+export default{
+    name: "AppFooter",
+    data(){
+      return{
+
+      }
+    }
+}
+</script>
+<template lang="">
+  <div class="sfondo">
+    <img src="/dc-logo-bg.png" alt="logo-dc" class="logo">
+  </div>
+</template>
+<style lang ="scss" scoped>
+.sfondo{
+    background-image: url("/footer-bg.jpg");
+}
+.logo{
+    width: 300px;
+    position: absolute;
+}
+.jumbo-footer{
+    width: 100%;
+    position: relavite
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+    name: "AppMain",
+    data(){
+      return{
+
+      }
+    }
+}
+</script>
+<template lang="">
+
+   
+        <div class="bg-nero">
+            <h3>Content goes here</h3>
+        </div>
+   
+
+
+</template>
+<style lang ="scss" scoped>
+.bg-nero{
+    background-color: black;
+    color: white;
+    align-items: center;
+    padding: 50px;
+
+}
+</style>
